d544b68496134435d5a1edc87685e11c3a0cc58f
Scaria <arunscaria91@gmail.com>
2016-04-20 14:35:49 -0700

    Login keboard scrolling added

--- Shift/LoginViewController.swift
+++ Shift/LoginViewController.swift
@@ -9,11 +9,14 @@
 import Foundation
 import SCLAlertView
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var emailText: UITextField!
     @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet var scrollView: UIScrollView!
+    var svos :CGPoint?
     @IBAction func loginBtn(sender: AnyObject) {
+        
         let loginService = LoginServices.sharedInstance
         loginService.authenticateUser(emailText.text!, password: passwordText.text!){ error, authData in
                 if error != nil {
@@ -25,6 +28,21 @@ class LoginViewController: UIViewController {
             
             }
         
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        svos = scrollView!.contentOffset;
+        var rc:CGRect = textField.bounds;
+        rc = textField.convertRect(rc, toView: scrollView);
+        var pt = rc.origin;
+        pt.x = 0;
+        pt.y =  pt.y - (UIScreen.mainScreen().bounds.height/4.0);
+        scrollView.setContentOffset(pt, animated: true)
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        scrollView.setContentOffset(svos!, animated: true)
+        textField.resignFirstResponder()
     }
     
     override func shouldAutorotate() -> Bool {
