bac44440cc01e65597dfce1b5f41c33a99cfb846
Meredith Moore <meredith.moore011@gmail.com>
2016-04-25 23:01:13 -0700

    added bounding box, improved control, still could be better though

--- Shift/Exercises.swift
+++ Shift/Exercises.swift
@@ -23,6 +23,7 @@ class Exercises: UIViewController {
     var target0, target1, target2, target3, target4, target5, target6, target7 : SKSpriteNode?
     var motionManager = CMMotionManager()
     var accX, accY, accZ, posX, posY: SKLabelNode?
+    var x, y: CGFloat?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,7 +44,7 @@ class Exercises: UIViewController {
         let scene = SKScene(fileNamed: "ExerciseScene")
         skView.presentScene(scene)
         view.addSubview(skView)
-    
+        
         
         //setup the player and targets (super gross code... but forcing it right now)
         player = skView.scene!.childNodeWithName("player") as? SKSpriteNode
@@ -55,7 +56,7 @@ class Exercises: UIViewController {
         target5 = skView.scene!.childNodeWithName("target5") as? SKSpriteNode
         target6 = skView.scene!.childNodeWithName("target6") as? SKSpriteNode
         target7 = skView.scene!.childNodeWithName("target7") as? SKSpriteNode
-
+        
         dot0 = skView.scene!.childNodeWithName("dot0") as? SKSpriteNode
         dot1 = skView.scene!.childNodeWithName("dot1") as? SKSpriteNode
         dot2 = skView.scene!.childNodeWithName("dot2") as? SKSpriteNode
@@ -73,7 +74,7 @@ class Exercises: UIViewController {
         target6!.hidden = true
         target7!.hidden = true
         dot0!.hidden = true
-
+        
         
         accX = skView.scene!.childNodeWithName("accX") as? SKLabelNode
         accY = skView.scene!.childNodeWithName("accY") as? SKLabelNode
@@ -87,13 +88,13 @@ class Exercises: UIViewController {
         
         
     }
-
+    
     
     func outputAccData(acceleration: CMAcceleration){
         
-        accX?.text = "\(acceleration.x).2fg"
-        accY?.text = "\(acceleration.y).2fg"
-        accZ?.text = "\(acceleration.z).2fg" //why won't this print?
+        //accX?.text = "\(acceleration.x).2fg"
+        //accY?.text = "\(acceleration.y).2fg"
+        //accZ?.text = "\(acceleration.z).2fg"
         print("Acceleration: (\(acceleration.x), \(acceleration.y), \(acceleration.z)")
     }
     
@@ -103,15 +104,46 @@ class Exercises: UIViewController {
         posY?.text = "\(player!.position.y).2fg"
         print("Player Position: \(player!.position.x) , \(player!.position.y)")
     }
-  
+    
     func movePlayer(acc: CMAcceleration){
         
         //need a bounding box
-        var currX = player!.position.x
-        var currY = player!.position.y
-        currX = currX - CGFloat(375 * acc.x)
-        currY = currY - CGFloat(667 * acc.y)
-
+        x = player!.position.x
+        y = player!.position.y
+        
+        //create a bounding box for the player to stay in. This should freeze it at the edges
+        if(x > 20){
+            if(x < 355){
+                if(y > 220){
+                    if(y < 585){
+                        x = x! + CGFloat(20 * acc.x)
+                        y = y! - CGFloat(20 * acc.z)
+                        
+                        player!.position = CGPoint(x: x!, y: y!)
+                        print("position changed")
+                    }
+                    else{
+                        print("top edge")
+                        y = 584
+                    }
+                    
+                }
+                else{
+                    print("bottom edge")
+                    y = 221
+                }
+                
+            }
+            else{
+                print("right edge")
+                x = 354
+            }
+            
+        }else{
+            print("left edge")
+            x = 21
+        }
+        
         outputPosData()
     }
     
@@ -123,7 +155,6 @@ class Exercises: UIViewController {
         //Start Recording Data
         motionManager.startAccelerometerUpdatesToQueue(NSOperationQueue.currentQueue()!) { (accelerometerData: CMAccelerometerData?, NSError) -> Void in
             
-            //probably put a if statement here
             if( accelerometerData != nil){
                 self.outputAccData(accelerometerData!.acceleration)
                 self.movePlayer(accelerometerData!.acceleration)
